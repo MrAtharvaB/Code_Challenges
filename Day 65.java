1ad899355b45ef3bdf2a279462ee96bc65f50016
Atharva Babhane <atharvastudy100@gmail.com>
2025-11-29 19:11:27 +0530

    Day 65

---
+++ Day 65.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int countSetBits(int n) {
+        if (n == 0)
+            return 0;
+        
+        int x = largestPowerOf2(n);
+        int bitsUpTo2x = x * (1 << (x - 1));
+        int msbBits = n - (1 << x) + 1;
+        int rest = countSetBits(n - (1 << x));
+        
+        return bitsUpTo2x + msbBits + rest;
+    }
+    
+    private static int largestPowerOf2(int n) {
+        int x = 0;
+        while ((1 << (x + 1)) <= n) {
+            x++;
+        }
+        return x;
+    }
+}
