1cbeb5f0da46ec7db95778ae2573feacb781dc5e
Atharva Babhane <atharvastudy100@gmail.com>
2026-05-20 15:43:31 +0530

    Day 236

---
+++ Day 236.java
@@ -0,0 +1,18 @@
+class Solution:
+    def isProduct(self, arr, target):
+        seen = set()
+
+        for num in arr:
+            if target == 0:
+                if num == 0 and seen:
+                    return True
+                if num != 0 and 0 in seen:
+                    return True
+            else:
+                if num != 0 and target % num == 0:
+                    if (target // num) in seen:
+                        return True
+
+            seen.add(num)
+
+        return False
