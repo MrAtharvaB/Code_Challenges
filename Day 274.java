d38d37e927a69b525d004315c4dc68a43d1df0e7
Atharva <atharvastudy100@gmail.com>
2026-06-27 19:16:13 +0530

    Day 274

---
+++ Day 274.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countWays(int n, int m) {
+        int MOD = 1000000007;
+
+        long[] dp = new long[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i < m)
+                dp[i] = 1;
+            else if (i == m)
+                dp[i] = 2;
+            else
+                dp[i] = (dp[i - 1] + dp[i - m]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
